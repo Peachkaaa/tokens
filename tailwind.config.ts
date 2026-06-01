36eedcfa458fdb82bb0b36c980ef17023e18edf9
LuciaFen <lucia.fenu@hubcore.ai>
2026-06-01 22:16:15 +0200

    add tailwind to build

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from 'tailwindcss';
+import { tokenColors } from './tailwind.tokens.generated';
 
 export default {
   content: [
@@ -11,20 +12,7 @@ export default {
   ],
   theme: {
     extend: {
-      colors: {
-        'token-gray-100': 'var(--primariMode1Gray100)',
-        'token-gray-700': 'var(--primariMode1Gray700)',
-        'token-red-100': 'var(--primariMode1Red100)',
-        'token-green-300': 'var(--primariMode1Green300)',
-        'token-yellow-300': 'var(--primariMode1Yellow300)',
-        'token-sky-600': 'var(--primariMode1Sky600)',
-        'token-border-grey-100': 'var(--tokensLightBorderBorderGrey100)',
-        'token-border-grey-700': 'var(--tokensLightBorderBorderGrey700)',
-        'token-button-red-100': 'var(--tokensLightButtonButtonRed100)',
-        'token-button-green-300': 'var(--tokensLightButtonButtonGreen300)',
-        'token-background-yellow-100': 'var(--tokensLightBackgroundBackgroundYellow100)',
-        'token-background-sky-600': 'var(--tokensLightBackgroundBackgroundSky600)'
-      }
+      colors: tokenColors
     }
   }
 } satisfies Config;
