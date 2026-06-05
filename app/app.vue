f3adb973630e1cc044b2872e87b1b166740d7724
LuciaFen <lucia.fenu@hubcore.ai>
2026-06-05 11:23:04 +0200

    fix path

--- app/app.vue
+++ app/app.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="p-10 bg-token-gray-100 flex gap-4">
-   <div class="h-20 w-20 bg-token-background-yellow-100"></div>
-   <div class="h-20 w-20 bg-token-border-grey-700"></div>
-   <div class="h-20 w-20 bg-token-background-sky-600"></div>
-   <div class="h-20 w-20 bg-token-button-red-100"></div>
+  <div class="p-10 bg-token-primary-mode1-blue100 flex gap-4">
+   <div class="h-20 w-20 bg-token-primary-mode1-violet100"></div>
+   <div class="h-20 w-20 bg-token-primary-mode1-yellow100"></div>
+   <div class="h-20 w-20 bg-token-primary-mode1-green100"></div>
+   <div class="h-20 w-20 bg-token-primary-mode1-red100"></div>
   </div>
 </template>
